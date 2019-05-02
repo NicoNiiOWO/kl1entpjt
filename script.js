ae2960bc965253637446f20e8415ccf474f9865e
Samuel Estevez <julioestevez@aclanyc.com>
2019-05-02 22:29:22 +0000

    aa

--- script.js
+++ script.js
@@ -2,14 +2,20 @@
 //"http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + input + "&api_key=8c1cf0cd4c49af664412691c079d2341&format=json"
 //https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=coda&api_key=8c1cf0cd4c49af664412691c079d2341&format=json
 
-$("button").click(function(){
-    var input = $("input").val();
-    var url = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + input + "&api_key=8c1cf0cd4c49af664412691c079d2341&format=json";
-    $.ajax({
-        url: url,
-        method: "GET",
-        success: function(response) {
-            console.log(response);
-        }
-    });
+var score = 0;
+$("#submit").click(function(){
+    // var input = $("input").val();
+    // var url = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + input + "&api_key=8c1cf0cd4c49af664412691c079d2341&format=json";
+    // $.ajax({
+    //     url: url,
+    //     method: "GET",
+    //     success: function(response) {
+    //         console.log(response);
+    //     }
+    // });
+    
+    if($("input[name='cardiovascularbronchitis']:checked").val() === "No"){
+        score+=1;
+    }
+    console.log(score);
 });
