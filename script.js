b14337fe7065c63b09a394e31b34d4efec3fa2b7
Samuel Estevez <julioestevez@aclanyc.com>
2019-05-02 21:47:02 +0000

    a

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /* global $ */
 //"http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + input + "&api_key=8c1cf0cd4c49af664412691c079d2341&format=json"
+//https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=coda&api_key=8c1cf0cd4c49af664412691c079d2341&format=json
 
 $("button").click(function(){
     var input = $("input").val();
-    var url = "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + input + "&api_key=8c1cf0cd4c49af664412691c079d2341&format=json";
+    var url = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + input + "&api_key=8c1cf0cd4c49af664412691c079d2341&format=json";
     $.ajax({
         url: url,
         method: "GET",
