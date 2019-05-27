0aeb10189092c9f61d06f47ed0728fe890af8d5d
Samuel Estevez <julioestevez@aclanyc.com>
2019-05-27 17:32:02 +0000

    epic

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 /* global $ */
+/* global tags */
 //"http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + input + "&api_key=8c1cf0cd4c49af664412691c079d2341&format=json"
 //https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=coda&api_key=8c1cf0cd4c49af664412691c079d2341&format=json
-const tags = ["A Cruel Angel's Thesis"];
-
+const body = $("body");
 
 function plswork(score){
-    if (0 <= score < 2){
-        return tags[0];
-    }
+    return tags[Math.floor(tags.length/(score+1))-1];
 }
 
 function value(name){
-    return $("input[name="+name+"]:checked").val()
+    return $("input[name="+name+"]:checked").val();
 }
 
 var score;
-function score(){
+function getScore(){
     score = 0;
     
     if(value("cardiovascularbronchitis") === "No"){
         score+=1;
     }
-    
+
     if(value("kpop") === "Yes"){
         score+=1;
     }
@@ -49,10 +47,22 @@ function score(){
 }
 
 var track;
+var videoId;
+var thumbnail;
 $("#submit").click(function(){
-    score();
+    getScore();
     var tag = plswork(score);
-    var url = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + tag + "&api_key=8c1cf0cd4c49af664412691c079d2341&format=json";
+    var name = tag.name;
+    var artist = tag.artist;
+    var url = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + name + "&api_key=8c1cf0cd4c49af664412691c079d2341&format=json";
+    if(artist !== ""){
+        url += ("&artist="+artist);
+    }
+    
+    
+    
+    body.empty();
+    
     $.ajax({
         url: url,
         method: "GET",
@@ -61,12 +71,24 @@ $("#submit").click(function(){
             console.log(response.results.trackmatches.track[0]);
         }
     }).then(function(){
-        var body = $("body")
-        body.empty();
         body.append("<p>Name: " + track.name + "</p>");
         body.append("<p>Artist: " + track.artist + "</p>");
-        body.append("<img src=" + track.image[0]["#text"] +"/>");
+        //body.append("<img src=" + track.image[0]["#text"] +"/>");
+        //body.append("<a href=" + track.url + ">URL</a>");
+        
+        $.ajax({
+            url: "https://www.googleapis.com/youtube/v3/search?key=AIzaSyAtTkre4m2Im0_WzgoaU99ROsMzT9zdUkg&part=snippet&q=" + name + " " + artist,
+            method: "GET",
+            success: function(response) {
+                console.log(response);
+                videoId = response.items[0].id.videoId;
+                thumbnail = response.items[0].snippet.thumbnails.medium.url;
+            }
+        }).then(function(){
+            var embed = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + videoId + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+            body.append(embed);
+        });
     });
-    
+
     
 });
---
+++ songs.js
@@ -0,0 +1,70 @@
+let tags = [{
+    name: "A Cruel Angel's Thesis",
+    artist: "Yoko Takahashi"
+}, {
+    name: "Unravel",
+    artist: "TK from 凛として時雨"
+}, {
+    name: "The Day",
+    artist: ""
+}, {
+    name: "Chase",
+    artist: "batta"
+}, {
+    name: "Fighting Gold",
+    artist: "Coda"
+}, {
+    name: "Hikaru Nara",
+    artist: "Goose house"
+}, {
+    name: "Snow Halation",
+    artist: "μ's"
+}, {
+    name: "Silhouette",
+    artist: "KANA-BOON"
+}, {
+    name: "Melissa",
+    artist: ""
+}, {
+    name: "Uragirimono no Requiem",
+    artist: "Daisuke Hasegawa"
+}, {
+    name: "Blue Bird",
+    artist: "いきものがかり"
+}, {
+    name: "Adabana Necromancy",
+    artist: "FranChouChou"
+}, {
+    name: "Guren no Yumiya",
+    artist: "Linked Horizon"
+}, {
+    name: "Hero's Comeback",
+    artist: "nobodyknows+"
+}, {
+    name: "Golden Time Lover",
+    artist: "スキマスイッチ"
+}, {
+    name: "Sono Chi No Kioku",
+    artist: "JOâ˜†STARS ~TOMMY,Coda,JIN~"
+}, {
+    name: "BLOODY STREAM",
+    artist: "Coda"
+}, {
+    name: "Sono Chi No Sadame",
+    artist: "富永TOMMY弘明"
+}, {
+    name: "Great Days",
+    artist: "青木カレン・ハセガワダイスケ"
+}, {
+    name: "Be Somewhere",
+    artist: "Buzy"
+}, {
+    name: "Redo",
+    artist: "鈴木このみ"
+}, {
+    name: "Touch off",
+    artist: "UVERworld"
+}, {
+    name: "Gurenge",
+    artist: "LiSa"
+}];
